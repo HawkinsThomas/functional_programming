f72ae02feee75e3486d7cf5d3dc39f82505e026c
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-04-06 19:04:28 -0400

    added a reduce example which reads a text file of customers and reduces it to a customer directory object.

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
-import {add} from './add';
+import fs from "fs"
 
+//reduce function
 
-console.log(add(1,2));
+let data = fs.readFileSync('data.txt', 'utf8')
+    .trim()
+    .split('\n')
+    .map(line => line.split(','))
+    .reduce(function (directory, line){
+        directory[line[0]] = directory[line[0]] || []
+        directory[line[0]].push({
+            name: line[1],
+            price: line[2],
+            quantity: line[3]
+        })
+        return directory
+    }, {})
 
-
-const person = {
-    name: "thomas",
+let person = {
+    name: "Thomas",
     age: 25,
-    email: "thomas.hawkins09@gmail.com"
+    address: {
+        street_name: "Broadway",
+        street_num: 120,
+        city: "Toronto",
+        province: "Ontario"
+    }
 }
 
-const address = {
-    street: "broadway",
-    city: "Toronto",
-    Province: "Ontario"
-}
+console.log(JSON.stringify(data, null, 2))
 
-const personWithAddress = {
-    ...person,
-    ...address
-}
+let animals = [
+    { name: "Fluffykins",   species: "rabbit" },
+    { name: "Caro",         species: "dog" },
+    { name: "Hamilton",     species: "dog" },
+    { name: "Harold",       species: "fish" },
+    { name: "Jimmy",        species: "cat" }
+]
 
-console.log(personWithAddress)
+let orders = [
+    {amount: 240},
+    {amount: 200},
+    {amount: 100},
+    {amount: 150},
+    {amount: 50}
+]
+
+let animal_name = (animal) => animal.name + " is a " + animal.species
+let total = orders.reduce(function(return_val, order) {
+    return (return_val + order.amount)
+}, 0)
+
+//console.log(total)
