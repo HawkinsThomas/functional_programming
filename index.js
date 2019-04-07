8affb216bd52eee4b89a838621fbbf92f49afda4
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-04-07 00:07:49 -0400

    added some recursion examples

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import fs from "fs"
 
-//reduce function
+//reduce example
 
 let data = fs.readFileSync('data.txt', 'utf8')
     .trim()
@@ -15,6 +15,8 @@ let data = fs.readFileSync('data.txt', 'utf8')
         })
         return directory
     }, {})
+
+//object literal
 
 let person = {
     name: "Thomas",
@@ -50,4 +52,124 @@ let total = orders.reduce(function(return_val, order) {
     return (return_val + order.amount)
 }, 0)
 
-//console.log(total)
+//recursion
+
+let count_down_from = (number) => {
+    if (number == 0){
+        return
+    }
+    else{
+        console.log(number)
+        count_down_from(number-1)
+    }
+}
+
+count_down_from(10)
+
+let categories = [
+    {id: 'animals', parent: null},
+    {id: 'mammals', parent: 'animals'},
+    {id: 'cats', parent: 'mammals'},
+    {id: 'dogs', parent: 'mammals'},
+    {id: 'chuhuahua', parent: 'dogs'},
+    {id: 'retreiver', parent: 'dogs'},
+    {id: 'persian', parent: 'cats'},
+    {id: 'tabby', parent: 'cats'}
+]
+
+let makeTree = (categories, parent) => {
+    let node = {}
+    categories
+        .filter(x => x.parent === parent)
+        .map(x => node[x.id] = makeTree(categories, x.id))
+    return node
+}
+
+console.log(
+    JSON.stringify(
+        makeTree(categories, null)
+        , null, 2)
+    )
+
+/*
+let makeTree = (categories) => {
+    categories.reduce((tree, item) => {
+        tree[item["id"]] = item["id"] || {}
+        console.log(tree[item["id"]])
+        return tree
+    }, {})
+}
+
+*/
+
+let make_tree = (categories, parent) => {
+    let node = {}
+    categories
+        .filter(x => x.parent === parent)
+        .forEach(x => node[x.id] = make_tree(categories, x.id)) 
+    return node
+}
+
+console.log(
+    JSON.stringify(
+        make_tree(categories, null)
+        , null, 2)
+    )
+let recursive_sum = (number) => {
+    if (number == 0){
+        return number 
+    }
+
+    let sum = number + recursive_sum(number - 1)
+    return sum
+}
+
+let array_test = [1,2,3,-10]
+
+let smallest = (array) => {
+    if (array.length == 2){
+        return array[0] < array[1] ? array[0] : array[1];
+    } 
+    let smallest_num = smallest(array.slice(1))
+    return array[0] < smallest_num ? array[0] : smallest_num;
+}
+
+let array_sum = (array) => {
+    if (array.length == 1){
+        return array[0]
+    }
+    return array[0] + array_sum(array.slice(1))
+}
+
+let isPalindrome = (s1) => {
+    console.log(s1)
+    if (s1.length == 2){
+        return s1[0] === s1[1]
+    }
+    else if (s1.length == 1){
+        return true
+    }
+    return (s1[0] === s1[s1.length -1] && isPalindrome(s1.slice(1,s1.length -1)))
+}
+    
+console.log(isPalindrome('racecar'))
+console.log(recursive_sum(10))
+console.log(smallest(array_test))
+console.log(array_sum(array_test))
+    
+/*
+{
+    animals: {
+        mammals: {
+            dogs:{
+                chihuahua: null
+                retreiver: null
+            }
+            cats:{
+                persian: null
+                tabby: null
+            }
+        }
+    }
+}
+*/
